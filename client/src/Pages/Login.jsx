7edfff89ec6f630dea0dbedc22e78ecbe7638072
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-03-10 00:39:27 +0100

    updt

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
     ev.preventDefault();
     try {
       const { data } = await axios.post("/login", { email, password });
+      console.log(data);
       localStorage.setItem("token", data.token);
       setUser(data);
       setRedirect(true);
