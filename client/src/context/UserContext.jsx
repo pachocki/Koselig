4ec30925ae56af00bf140b0327d10ddeee111509
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-03-10 11:41:46 +0100

    vercel

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,22 +1,29 @@
-import {createContext, useEffect, useState} from "react";
-import axios from "axios";
-import {data} from "autoprefixer";
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
 
-export const UserContext = createContext({});
-
-export function UserContextProvider({children}) {
-  const [user,setUser] = useState(null);
-  const [ready,setReady] = useState(false);
   useEffect(() => {
-    if (!user) {
-      axios.get('/profile').then(({data}) => {
-        setUser(data);
-        setReady(true);
-      });
+    const token = Cookies.get('token') || localStorage.getItem('token');
+    if (token) {
+      axios
+        .get('/profile', {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(({ data }) => {
+          setUser(data);
+          setReady(true);
+        })
+        .catch((error) => {
+          console.log('Error fetching user data:', error);
+          setReady(true);
+        });
+    } else {
+      setReady(true);
     }
   }, []);
+
   return (
-    <UserContext.Provider value={{user,setUser,ready}}>
+    <UserContext.Provider value={{ user, setUser, ready }}>
       {children}
     </UserContext.Provider>
   );
