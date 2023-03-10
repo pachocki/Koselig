28d1ac2bbf079292e0a8bb712b3d70b9db642a02
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-03-10 01:22:08 +0100

    updt

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,41 +1,70 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import Image from "../assets/login.webp";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import Cookies from "js-cookie";
 import { UserContext } from "../context/UserContext";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [redirect, setRedirect] = useState(false);
-  const { setUser } = useContext(UserContext);
-  async function handleLoginSubmit(ev) {
-    ev.preventDefault();
-    try {
-      const { data } = await axios.post("/login", { email, password }, { withCredentials: true });
-      const token = data.token;
-      localStorage.setItem("token", token);
-      setUser(data);
-      setRedirect(true);
-    } catch (e) {
-      alert("Login failed");
-    }
-  }
+  const [errMsg, setErrMsg] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { user,setUser } = useContext(UserContext);
+  const errRef = useRef();
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, password]);
 
-  if (redirect) {
-    return <Navigate to={"/"} />;
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const { data } = await axios.post(
+        "/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+  
+      if (data && data.user && data.user._id) {
+        const token = data.token;
+        localStorage.setItem("token", token);
+        setIsAuthenticated(true);
+        setUser(data.user);
+        navigate("/");
+      
+  
+      } else {
+        setErrMsg("Login Failed");
+        errRef.current.focus();
+      }
+    } catch (error) {
+      if (!error?.response) {
+        console.log(error);
+        setErrMsg("No server response");
+      } else if (error.response?.status === 400) {
+        setErrMsg("Missing username or password");
+      } else if (error.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+  useEffect(() => {
+    console.log("User updated:", user);
+  }, [user]);
 
   return (
     <div className="w-full h-screen flex justify-center items-center pt-20 lg:px-2">
       <div className="flex justify-center shadow-lg overflow-hidden xs:w-full">
         <div className="w-1/2 lg:w-1/3 xs:hidden">
-          <img
-            src={Image}
-            alt="mountains"
-            className="w-full h-full object-cover"
-          />
+          <img src={Image} alt="mountains" className="w-full h-full object-cover" />
         </div>
         <div className="flex flex-col justify-center w-1/2 border-2 lg:w-2/3 xs:w-full">
           <h2 className="text-center pb-5 text-4xl  lg:text-2xl lg:pt-5 sm:text-[1.1rem] sm:font-bold">
@@ -44,11 +73,9 @@ const Login = () => {
           <div className="flex justify-center items-center xs:w-full">
             <form
               className="flex flex-col gap-2 w-2/3 overflow-hidden lg:w-[95%] "
-              onSubmit={handleLoginSubmit}
+              onSubmit={handleLogin}
             >
-              <label className="text-xl text-gray-500  lg:text-lg sm:text-sm">
-                E-mail
-              </label>
+              <label className="text-xl text-gray-500  lg:text-lg sm:text-sm">E-mail</label>
               <input
                 type="email"
                 placeholder="your@email.com"
@@ -56,9 +83,7 @@ const Login = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-              <label className="text-xl text-gray-500 lg:text-lg sm:text-sm">
-                Password
-              </label>
+              <label className="text-xl text-gray-500 lg:text-lg sm:text-sm">Password</label>
               <input
                 type="password"
                 placeholder="password"
@@ -75,7 +100,13 @@ const Login = () => {
                   Register
                 </Link>
               </span>
-             
+              <p
+                ref={errRef}
+                className={errMsg ? "block text-red-600 text-center" : "hidden"}
+                aria-live="assertive"
+              >
+                {errMsg}
+              </p>
             </form>
           </div>
         </div>
