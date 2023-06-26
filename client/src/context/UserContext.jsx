13c64445399c1f1d8ec88e5219a6c381467c0b53
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-06-26 20:14:11 +0200

    Update

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,31 +1,20 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import Cookies from "js-cookie";
+import { data } from "autoprefixer";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const [ready, setReady] = useState(false);
-
   useEffect(() => {
-    const token = Cookies.get("token");
-    if (token) {
-      axios
-        .get("/api/profile", { headers: { Authorization: `Bearer ${token}` } })
-        .then(({ data }) => {
-          setUser(data);
-          setReady(true);
-        })
-        .catch((error) => {
-          console.log("Error fetching user data:", error);
-          setReady(true);
-        });
-    } else {
-      setReady(true);
+    if (!user) {
+      axios.get("/profile").then(({ data }) => {
+        setUser(data);
+        setReady(true);
+      });
     }
   }, []);
-
   return (
     <UserContext.Provider value={{ user, setUser, ready }}>
       {children}
